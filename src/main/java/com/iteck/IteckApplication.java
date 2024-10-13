0beb828d02a422775b272c79f8242f88cc67ddc6
박진영 <am030813@naver.com>
2024-10-13 17:01:49 +0900

    initial commit

---
+++ src/main/java/com/iteck/IteckApplication.java
@@ -0,0 +1,13 @@
+package com.iteck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IteckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IteckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iteck/IteckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iteck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IteckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
