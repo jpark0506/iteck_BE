1ae11e4b8b856890a128962428b22255d68dcfb1
박진영 <am030813@naver.com>
2024-10-28 07:24:55 +0900

    FEAT: 실험 데이터 레포지토리 인터페이스

--- src/main/java/com/iteck/repository/ExperimentMetaRepository.java
+++ src/main/java/com/iteck/repository/ExperimentMetaRepository.java
@@ -1,9 +1,12 @@
 package com.iteck.repository;
 
-import com.iteck.domain.Experiment;
+import com.iteck.domain.ExperimentMeta;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface ExperimentRepository extends MongoRepository<Experiment, String> {
+public interface ExperimentMetaRepository extends MongoRepository<ExperimentMeta, String> {
+    public List<ExperimentMeta> findByUserName(String userName);
 }
