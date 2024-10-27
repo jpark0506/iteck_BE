18466faab6e3d1fad5c150d3c93b8be84f2f17cd
박진영 <am030813@naver.com>
2024-10-27 22:06:10 +0900

    FEAT: 실험 데이터 저장 로직

--- src/main/java/com/iteck/service/FileService.java
+++ src/main/java/com/iteck/service/FileService.java
@@ -1,45 +1,58 @@
 package com.iteck.service;
 
 import com.iteck.domain.ExcelData;
+import com.iteck.domain.Experiment;
+import com.iteck.dto.ApiResponse;
 import com.iteck.repository.ExcelDataRepository;
+import com.iteck.repository.ExperimentRepository;
+import com.iteck.util.ApiStatus;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class FileService {
-    @Autowired
-    private final ExcelDataRepository excelDataRepository;
-    public List<ExcelData> createExcelData(MultipartFile file) throws IOException {
-        Workbook workbook = new XSSFWorkbook(file.getInputStream());
-        Sheet sheet = workbook.getSheetAt(0);
-        List<ExcelData> excelDataList = new ArrayList<>();
-        for (Row row : sheet) {
-            List<String> rowData = new ArrayList<>();
-            for (Cell cell : row) {
-                rowData.add(cell.toString());
-            }
-            ExcelData excelData = new ExcelData();
-            excelData.setRowData(rowData);
-            excelDataList.add(excelData);
+    private final ExperimentRepository experimentRepository;
+    public ApiResponse<?> createExperimentData(MultipartFile file) throws IOException {
+        // CSV 파일인지 확인
+        if (!"text/csv".equals(file.getContentType()) && !"application/vnd.ms-excel".equals(file.getContentType())) {
+            return ApiResponse.fromResultStatus(ApiStatus.BAD_REQUEST);
         }
-        workbook.close();
-        // MongoDB에 저장
-        excelDataRepository.saveAll(excelDataList);
 
-        return excelDataList;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()));
+        String line;
+        List<Experiment> experiments = new ArrayList<>();
+
+        while ((line = reader.readLine()) != null) {
+            String[] data = line.split(",");
+            Experiment experiment = new Experiment();
+            experiment.setRowData(Arrays.asList(data));
+            experiments.add(experiment);
+        }
+
+        // MongoDB에 저장
+        experimentRepository.saveAll(experiments);
+        System.out.println("success");
+        return ApiResponse.fromResultStatus(ApiStatus.SUC_EXPERIMENT_CREATE, experiments);
     }
-    public List<ExcelData> readExcelData(){return excelDataRepository.findAll();}
+
+    public List<Experiment> readExcelData(){ return experimentRepository.findAll();}
 }
