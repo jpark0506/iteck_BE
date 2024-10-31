16f71236b918a495f7aa19bc1dad367f64987ce8
박진영 <am030813@naver.com>
2024-10-31 23:32:53 +0900

    FIX: 메타 데이터 레포

--- src/main/java/com/iteck/repository/ExperimentMetaRepository.java
+++ src/main/java/com/iteck/repository/ExperimentMetaRepository.java
@@ -2,6 +2,7 @@ package com.iteck.repository;
 
 import com.iteck.domain.ExperimentMeta;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +10,7 @@ import java.util.List;
 @Repository
 public interface ExperimentMetaRepository extends MongoRepository<ExperimentMeta, String> {
     public List<ExperimentMeta> findByUserName(String userName);
+    @Query("{ 'factors.?0' : { $exists: true } }")
+    public List<ExperimentMeta> findByDynamicFactorKey(String key);
+
 }
