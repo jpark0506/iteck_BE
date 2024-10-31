3327c727dda1af82b87e2b1a00c9ee67399e8441
박진영 <am030813@naver.com>
2024-10-31 23:29:27 +0900

    FEAT: 시간 데이터 엔티티

---
+++ src/main/java/com/iteck/domain/TimeData.java
@@ -0,0 +1,24 @@
+package com.iteck.domain;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+import java.util.Map;
+
+@Document(collection = "time_data")
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TimeData {
+    @Id
+    private String id;
+    @Indexed
+    private String experimentId; // 실험 ID
+    private Integer cycleIndex;
+    private int chunkId; // 청크 ID (1, 2, 3, ...)
+    private List<Map<String, Object>> expSpec;
+}
