6675109868a54363f6d2fdba48a82cc137d518ca
박진영 <am030813@naver.com>
2024-10-31 23:31:54 +0900

    FIX: 시간 데이터 레포

--- src/main/java/com/iteck/repository/TimeDataRepository.java
+++ src/main/java/com/iteck/repository/TimeDataRepository.java
@@ -1,2 +1,14 @@
-package com.iteck.repository;public class TimeDataRepository {
+package com.iteck.repository;
+
+import com.iteck.domain.CycleData;
+import com.iteck.domain.TimeData;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TimeDataRepository extends MongoRepository<TimeData, String> {
+    List<TimeData> findByExperimentId(String experimentId);
 }
+
