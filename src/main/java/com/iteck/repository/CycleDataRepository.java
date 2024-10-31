4c17f11a6128deca587590a94a2b1e6e4fa34e5a
박진영 <am030813@naver.com>
2024-10-31 23:31:40 +0900

    FIX: 사이클 데이터 레포

--- src/main/java/com/iteck/repository/CycleDataRepository.java
+++ src/main/java/com/iteck/repository/CycleDataRepository.java
@@ -1,2 +1,13 @@
-package com.iteck.repository;public interface CycleDataRepository {
+package com.iteck.repository;
+
+import com.iteck.domain.CycleData;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CycleDataRepository extends MongoRepository<CycleData, String> {
+
+    List<CycleData> findByExperimentId(String experimentId);
 }
