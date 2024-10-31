856a77e34ee79d80a0263f5ab56ae4ceb13a1fe2
박진영 <am030813@naver.com>
2024-11-01 03:17:39 +0900

    FEAT: exclude 추가

--- src/main/java/com/iteck/IteckApplication.java
+++ src/main/java/com/iteck/IteckApplication.java
@@ -2,12 +2,12 @@ package com.iteck;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class IteckApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(IteckApplication.class, args);
 	}
-
 }
