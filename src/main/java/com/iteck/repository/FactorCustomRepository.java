f070b5d3347cd7ee20305253962b7eab1bdbd2b8
박진영 <am030813@naver.com>
2024-11-06 08:35:50 +0900

    FIX: 실험 인자 정보와 메타데이터 분리

--- src/main/java/com/iteck/repository/FactorCustomRepository.java
+++ src/main/java/com/iteck/repository/FactorCustomRepository.java
@@ -1,6 +1,6 @@
 package com.iteck.repository;
 
-import com.iteck.domain.ExperimentMeta;
+import com.iteck.domain.Factor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
@@ -10,18 +10,18 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public class ExperimentMetaCustomRepository {
+public class FactorCustomRepository {
 
     @Autowired
     private MongoTemplate mongoTemplate;
-
-    public List<ExperimentMeta> findByFactorKeyExists(String kind, String key) {
+    public List<Factor> findByFactorKeyExists(String kind, String key) {
         String fieldPath = "factors." + kind + ".details." + key;
 
         // 해당 경로에 키가 존재하는 문서를 조회
         Query query = new Query(Criteria.where(fieldPath).exists(true));
 
-        return mongoTemplate.find(query, ExperimentMeta.class);
+        return mongoTemplate.find(query, Factor.class);
     }
+
 }
 
--- src/main/java/com/iteck/repository/FactorRepository.java
+++ src/main/java/com/iteck/repository/FactorRepository.java
@@ -1,18 +1,14 @@
 package com.iteck.repository;
 
-import com.iteck.domain.ExperimentMeta;
+import com.iteck.domain.Factor;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface ExperimentMetaRepository extends MongoRepository<ExperimentMeta, String> {
-    public List<ExperimentMeta> findByUserName(String userName);
-    public List<ExperimentMeta> findAllByTitle(String title);
-    ExperimentMeta findFirstByTitle(String title);
-    String findExperimentIdByTitle(String title);
+public interface FactorRepository extends MongoRepository<Factor, String> {
     void deleteByExperimentId(String experimentId);
+    List<Factor> findByUserName(String userName);
 
 }
