ec6d53f235aa02a26ea45503fed734bd9456cfa8
박진영 <am030813@naver.com>
2024-11-06 08:33:53 +0900

    FIX: 실험 인자 정보와 메타데이터 분리

--- src/main/java/com/iteck/dto/MetaDto.java
+++ src/main/java/com/iteck/dto/MetaDto.java
@@ -1,16 +1,26 @@
 package com.iteck.dto;
 
 import com.iteck.domain.FactorDetail;
+import com.iteck.domain.Meta;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Map;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class MetaDto {
     private String userName;
     private String title;
     private String memo;
-    private Map<String, FactorDetail> factors;
+
+    public MetaDto(Meta meta) {
+        this.userName = meta.getUserName();
+        this.title = meta.getTitle();
+        this.memo = meta.getMemo();
+    }
 }
--- src/main/java/com/iteck/dto/ResponseDto.java
+++ src/main/java/com/iteck/dto/ResponseDto.java
@@ -1,8 +1,6 @@
 package com.iteck.dto;
 
-import com.iteck.domain.CycleData;
-import com.iteck.domain.ExperimentMeta;
-import com.iteck.domain.TimeData;
+import com.iteck.domain.Factor;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,35 +14,35 @@ public class ResponseDto {
     @Builder
     @AllArgsConstructor
     public static class TimeWithVoltage{
-        private ExperimentMeta meta;
+        private Factor meta;
         private Map<String, List<TimeDto.withVoltage>> timeDatas;
     }
     @Getter
     @Builder
     @AllArgsConstructor
     public static class TimeWithCurrent{
-        private ExperimentMeta meta;
+        private Factor meta;
         private Map<String, List<TimeDto.withCurrent>> timeDatas;
     }
     @Getter
     @Builder
     @AllArgsConstructor
     public static class CycleDchgToChg{
-        private ExperimentMeta meta;
+        private Factor meta;
         private Map<String, List<CycleDto.dchgToChg>> cycleDatas;
     }
     @Getter
     @Builder
     @AllArgsConstructor
     public static class CycleChgToDchg{
-        private ExperimentMeta meta;
+        private Factor meta;
         private Map<String, List<CycleDto.chgToDchg>> cycleDatas;
     }
     @Getter
     @Builder
     @AllArgsConstructor
     public static class VoltageWithDQmDv{
-        private ExperimentMeta meta;
+        private Factor meta;
         private Map<String, List<VoltageDto.withDQDV>> voltageDatas;
     }
 }
